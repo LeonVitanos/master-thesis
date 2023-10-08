b821fe97b8880481100b2fa762253bdd2c1f5ebe
Leon Vitanos <leon.vitanos@gmail.com>
2023-10-08 15:27:33 +0200

    Updates

---
+++ precache-manifest.fb6f74c449812bae56f92355b76efa48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61fd61b8c63cc5c7363a",
+    "url": "/master-thesis/static/css/main.0b346d46.chunk.css"
+  },
+  {
+    "revision": "61fd61b8c63cc5c7363a",
+    "url": "/master-thesis/static/js/main.4cee930f.chunk.js"
+  },
+  {
+    "revision": "ec4de095b49f7a6a478f",
+    "url": "/master-thesis/static/js/runtime~main.2be97c03.js"
+  },
+  {
+    "revision": "f0a03df69ebfe701a933",
+    "url": "/master-thesis/static/js/2.865b8baf.chunk.js"
+  },
+  {
+    "revision": "b81de5217954e9673f4eac174ebf708b",
+    "url": "/master-thesis/index.html"
+  }
+];
